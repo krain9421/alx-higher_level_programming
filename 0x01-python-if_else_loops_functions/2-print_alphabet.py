2806a685e204e020738674520e906aac6d6a0836
Nwigwe Chidubem <krain9421@gmail.com>
2022-11-29 19:04:39 +0000

    Added .py file

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 num = 0
 while num < 1:
-    print("{}".format("abcdefghijklmnopqrstuvwxyz"), end = "")
+    print("{}".format("abcdefghijklmnopqrstuvwxyz"), end="")
     num = 1
