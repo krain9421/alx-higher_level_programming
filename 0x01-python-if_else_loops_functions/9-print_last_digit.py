a9c341f721aad6a99edc3d69b10d70353f9e5155
Nwigwe Chidubem <krain9421@gmail.com>
2022-11-29 19:26:18 +0000

    Added .py file

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        return ((number * -1) % 10)
+        last = (number * -1) % 10
     else:
-        return (number % 10)
+        last = number % 10
+
+    print(last)
+    return (last)
