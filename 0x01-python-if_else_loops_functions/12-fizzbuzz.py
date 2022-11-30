70404416eeac254d5b3e80801cbe390cb3588469
Nwigwe Chidubem <krain9421@gmail.com>
2022-11-30 04:45:27 +0000

    Added .py file

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    m3 = i % 3
-    m5 = i % 5
-    if m3 == 0 and m5 != 0:
-        print("Fizz", end=" ")
-    elif m5 == 0 and m3 != 0:
-        if i == 100:
-            print("Buzz")
-        else:
-            print("Buzz", end=" ")
+def fizzbuzz():
+    for i in range(1, 101):
+        m3 = i % 3
+        m5 = i % 5
+        if m3 == 0 and m5 != 0:
+            print("Fizz", end=" ")
+        elif m5 == 0 and m3 != 0:
+            if i == 100:
+                print("Buzz")
+            else:
+                print("Buzz", end=" ")
 
-    elif m5 == 0 and m3 == 0:
-        print("FizzBuzz", end=" ")
-    else:
-        print(i, end=" ")
+        elif m5 == 0 and m3 == 0:
+            print("FizzBuzz", end=" ")
+        else:
+            print(i, end=" ")
