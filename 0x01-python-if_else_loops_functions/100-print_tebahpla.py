4e6729d2d68dee3f1a928244c0a45a878b0b59d2
Nwigwe Chidubem <krain9421@gmail.com>
2022-12-01 04:42:09 +0000

    Added .py file

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 for c in range(122, 96, -1):
-    if c % 2 != 0:
-        print(chr(c-32), end="")
-    else:
-        print(chr(c), end="")
+    i = c % 2
+    print("{}".format(chr(c-32) if i != 0 else (chr(c))), end="")
