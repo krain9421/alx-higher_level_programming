b451646e166c90d5b32ecac603e24849d9ee6299
Nwigwe Chidubem <krain9421@gmail.com>
2022-12-01 04:14:34 +0000

    Added .py file

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+n1 = 48
+n2 = 49
+for i in range(0, 10):
+    for j in range(0, 9):
+        if n1 >= n2 + j:
+            pass
+        else:
+            print("{}{}".format(chr(n1), chr(n2 + j)), end="")
+            if n1 == 56 and j == 8:
+                pass
+            else:
+                print(", ", end="")
+    n1 = n1 + 1
+print("")
