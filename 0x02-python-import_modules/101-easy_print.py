cdf5f7cd1afb526ec1b6e0bd2744d073018c6cf5
Nwigwe Chidubem <krain9421@gmail.com>
2022-12-02 03:40:50 +0000

    Added .py file

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def main():
+    import logging
+    logging.basicConfig(format='%(message)s', level=logging.INFO)
+    logging.info('#pythoniscool')
+
+
+if __name__ == "__main__":
+    main()
