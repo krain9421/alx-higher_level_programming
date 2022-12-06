8d168890551c2280fc55a7af74fe57868d8cb408
Nwigwe Chidubem <krain9421@gmail.com>
2022-12-06 00:13:42 +0000

    Added 10-divisible_by_2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    list_2 = []
+    for i in my_list:
+        if i % 2 == 0:
+            list_2.append(True)
+        else:
+            new_list.append(False)
+    return new_list
