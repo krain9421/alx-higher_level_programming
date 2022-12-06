5698806d343d3dc97e6e6335ede9186e03d29ab8
Nwigwe Chidubem <krain9421@gmail.com>
2022-12-06 00:04:50 +0000

    Added 9-max_integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if len(my_list) != 0:
-        big = 0
+        nmax = my_list[0]
         for i in my_list:
-            if i > big:
-                big = i
-        return big
+            if i > nmax:
+                nmax = i
+        return nmax
     else:
         return None
