203addc18cdba1b5cbe78bac2e078d2d178a70b1
Nwigwe Chidubem <krain9421@gmail.com>
2022-12-07 14:23:19 +0000

    Added 3-common_elements

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,5 +2,5 @@
 # A function that returns a set
 #   of common elements in two sets.
 def common_elements(set_1, set_2):
-    new_set = {x for x in set_1 & set_2)
+    new_set = {x for x in set_1 & set_2}
     return new_set
