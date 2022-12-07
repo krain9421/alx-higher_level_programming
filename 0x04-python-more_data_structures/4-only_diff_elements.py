9ec99a1bfd7a981707f40c4c540b178449869dcd
Nwigwe Chidubem <krain9421@gmail.com>
2022-12-07 14:27:07 +0000

    Added 4-only_diff_elements

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # A function that returns a set
 #   of all elements present in only one set.
-def common_elements(set_1, set_2):
+def only_diff_elements(set_1, set_2):
     new_set = {x for x in set_1 ^ set_2}
     return new_set
