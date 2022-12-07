4316ec09a6f8118aaae827c8de44a849de1491b8
Nwigwe Chidubem <krain9421@gmail.com>
2022-12-07 05:18:53 +0000

    Added 0-square_matrix_simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# A function that computes the square value of all integers of a matrix.
+def square_matrix_simple(matrix=[]):
+    new_matrix = [[i ** 2 for i in row] for row in matrix]
+    return new_matrix
