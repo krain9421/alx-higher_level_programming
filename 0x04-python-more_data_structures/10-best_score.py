a5c38a6e5e62f2e441a125d51960ab092e254d02
Nwigwe Chidubem <krain9421@gmail.com>
2022-12-08 01:58:15 +0000

    Added 10-best_score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# A function that returns a key
+#   with the biggest integer value.
+def best_score(a_dictionary):
+    key_max = None
+    val_max = float(-inf)
+    # Loop through the items in the dictionary
+    for key, val in a_dictionary.items():
+        # If the value is greater than the current maximum,
+        #   update the maximum and the key
+        if val > val_max:
+            val_max = val
+            key_max = key
+    # Return the key with the maximum value
+    return key_max
