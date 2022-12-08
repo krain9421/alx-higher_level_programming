10d2812e0306c6f6889617a3e83386b0ee1407cc
Nwigwe Chidubem <krain9421@gmail.com>
2022-12-08 01:36:48 +0000

    Added 9-multiply_by_2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,11 @@
 # A function that returns
 #   a new dictionary with all values multiplied by 2
 def multiply_by_2(a_dictionary):
+    # Dictionary to store the multiplied values
+    multiplied = {}
+    # Loop through the items in the original dictionary
     for key, val in a_dictionary.items():
-        a_dictionary[key] = val * 2
+        # Multiply the value by 2 and store it
+        multiplied[key] = value * 2
+    # Return the new dictionary
     return a_dictionary
