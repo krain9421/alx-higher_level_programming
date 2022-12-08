dc4775d2de42f0911db1bd5b9feebf08dcd29b15
Nwigwe Chidubem <krain9421@gmail.com>
2022-12-08 00:58:25 +0000

    Added 7-update_dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,4 +2,5 @@
 # A function that replaces or
 #   adds key/value in a dictionary.
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    if key:
+        a_dictionary[key] = value
