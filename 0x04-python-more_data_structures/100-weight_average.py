c6bd466aa71e15fed6af488ce03e6b6476c2daa9
Nwigwe Chidubem <krain9421@gmail.com>
2022-12-08 03:27:13 +0000

    Added 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# A function that returns the weighted
+#   average of all integers tuple (<score>, <weight>)
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    else:
+        score_sum = 0
+        weight_sum = 0
+        for row in my_list:
+            weight_sum += row[0] * row[1]
+            score_sum += row[1]
+        return (weight_sum//score_sum)
