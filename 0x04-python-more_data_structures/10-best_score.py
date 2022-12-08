cfc25cf22f6a3132a3104d4be5600d8cc9998159
Nwigwe Chidubem <krain9421@gmail.com>
2022-12-08 02:52:34 +0000

    Added 10-best_score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,19 +6,23 @@ def best_score(a_dictionary):
     val_max = 0
     count = 0
     # Check if dictionary is None
-    if a_dictionary == None:
+    if not a_dictionary:
         return None
     # Loop through the items in the dictionary
     for key, val in a_dictionary.items():
         # If count is 0, update maximum value
         if count == 0:
             val_max = val
-            key_max = key
+            if key:
+                key_max = key
         # If the value is greater than the current maximum,
         #   update the maximum and the key
         if val > val_max:
             val_max = val
-            key_max = key
+            if key:
+                key_max = key
+            else:
+                key_max = None
         # Update value of count
         count += 1
     # Return the key with the maximum value
