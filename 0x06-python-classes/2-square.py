e2a6fe43841e9d66dae327cf996fb0305e968431
krain9421 <krain9421@gmail.com>
2022-12-21 03:12:41 +0000

    Added 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Square class that defines a square."""
+
+
+class Square:
+    """Representation of square
+
+    Args:
+        size (int): The size of the new square.
+    """
+
+    def __init__(self, size=0):
+        try:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            size += 1
+            size -= 1
+        except TypeError:
+            raise TypeError("size must be an integer")
+        finally:
+            self.__size = size
