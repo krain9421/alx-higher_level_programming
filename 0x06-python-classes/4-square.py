6cb7257e1615a8267a42d31cc776edc9f3139064
krain9421 <krain9421@gmail.com>
2022-12-21 03:48:33 +0000

    Added 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Square class that defines a square."""
+
+
+class Square:
+    """Representation of square
+
+    Args:
+        size (int): The size of the new square.
+    """
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Set the current size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the current area of the square."""
+        return (self.__size ** 2)
