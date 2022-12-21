a7fe9ca2cf34285c8adafbc549725b6447a4aecb
krain9421 <krain9421@gmail.com>
2022-12-21 02:59:08 +0000

    Added 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square class that defines a square"""
+
+
+class Square:
+    """Representation of square
+
+    Args:
+        size (int): The size of the new square.
+    """
+
+    def __init__(self, size):
+        self.__size = size
