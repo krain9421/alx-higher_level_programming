dc81f688e19c6ac5fa2212cbcd324433637cecf2
krain9421 <krain9421@gmail.com>
2023-01-05 04:11:05 +0000

    commit

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -13,6 +13,8 @@ class Rectangle:
         height (int): The height of the new rectangle
     """
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         type(self).number_of_instances += 1
         self.width = width
