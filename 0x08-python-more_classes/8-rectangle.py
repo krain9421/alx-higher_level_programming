26ca7ef05fd96406109d1ec760e25dbea63840d8
krain9421 <krain9421@gmail.com>
2023-01-05 04:38:19 +0000

    commit

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -92,7 +92,7 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
 
-        if rect_2.area > rect_1.area:
+        if rect_2.area() > rect_1.area():
             return (rect_2)
         else:
             return (rect_1)
