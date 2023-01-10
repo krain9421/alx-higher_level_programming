e4c6f59a6fe88a07c89e186457546eda4e32b127
krain9421 <krain9421@gmail.com>
2023-01-10 04:26:07 +0000

    commit

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function that checks that inherited (directly or indirectly) from
+    the specified class
+"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True if ``obj`` is an instance of 
+    a class that inherited from ``a_class``
+
+    Args:
+        obj(object): The object to be checked.
+        a_class (class): The specified class.
+    """
+    return issubclass(obj, a_class)
