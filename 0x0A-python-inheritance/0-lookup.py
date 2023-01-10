c5804bdb4522215d1216462ecc9048b3b2050e01
krain9421 <krain9421@gmail.com>
2023-01-10 03:56:48 +0000

    commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,5 +3,10 @@
 
 
 def lookup(obj):
+    """Returns the list of attribute and methods
+
+    Args:
+        obj(object): The object to be checked.
+    """
     list_of_attr = dir(obj)
     return (list_of_attr)
