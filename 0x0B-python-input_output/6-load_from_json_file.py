613d8c766df36dca1ac0c96baf4bee0d07aad827
krain9421 <krain9421@gmail.com>
2023-01-11 14:41:53 +0000

    commit

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Function that creates an Object from a JSON file."""
+import json
+
+
+def load_from_json_file(filename):
+    """Creates an Object from a 'JSON file'
+
+    Args:
+        filename(string): text file
+    """
+
+    with open(filename, "w") as myFile:
+        my_obj = json.load(myFile)
+        return (my_obj)
