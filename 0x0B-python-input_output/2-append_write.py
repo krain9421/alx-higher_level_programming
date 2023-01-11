a06d98e10ecea8a8b15a06e320eea21a67b1f9cb
krain9421 <krain9421@gmail.com>
2023-01-11 09:40:11 +0000

    commit

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Function that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text and returns the number of
+        characters written.
+
+    Args:
+        filename(string): name of the file to be written.
+        text(string): text to be written to the file. 
+    """
+
+    with open(filename, 'w', encoding="utf-8") as myFile:
+        """Printing the contents of the file"""
+        return (myFile.write(text))
