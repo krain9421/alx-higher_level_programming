e476dac63988a406414efd6696ced7ff6e4a9c84
krain9421 <krain9421@gmail.com>
2023-01-13 19:15:50 +0000

    Added base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This model defines a base class for all the classes in
+    the project
+"""
+
+
+class Base:
+    """Defines a Base class"""
+
+    # Private class attribute
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initializes a Base object
+
+        Args:
+            id (int): id of the object
+        """
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
