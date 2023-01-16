6c35dd2eeee8f011fdd7a11e3c65760630afbdcd
krain9421 <krain9421@gmail.com>
2023-01-16 16:50:37 +0000

    Modified models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,9 +16,8 @@ class Base:
         Args:
             id (int): id of the object
         """
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
