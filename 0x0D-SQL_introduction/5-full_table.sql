3384592b04bec4255ed5c4f1da4be0b77201bc7d
Nwigwe Chidubem <krain9421@gmail.com>
2023-02-14 20:40:18 +0000

    Added file

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- A script that prints the full description of the table `firs_table`
+-- from the database hbtn_0c_0
+-- The database name will be passed as an argument of the mysql command
+SHOW CREATE TABLE `first_table`;
