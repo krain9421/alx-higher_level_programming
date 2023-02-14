9105ff6b773a08c7ccd98d8f17eeb39185a36024
Nwigwe Chidubem <krain9421@gmail.com>
2023-02-14 20:23:03 +0000

    Added file

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- A script that deletes the database `hbtn_0c_0` in your SQL server
+-- If the databses `hbtn_0c_0` doesn't exist, the scipt
+-- should not fail
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
