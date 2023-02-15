46838b7d881b41e36c2cd0939ee288bb5636e193
Nwigwe Chidubem <krain9421@gmail.com>
2023-02-15 04:05:05 +0000

    Added file

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- A script that lists all records of the table `second_table` by `score`
+-- and `name` sorted in descending order
+SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
