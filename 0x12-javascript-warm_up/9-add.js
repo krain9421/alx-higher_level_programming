f0304beb34c9a3a3e0b2a7a2333555853b3ce2ee
Nwigwe Chidubem <krain9421@gmail.com>
2023-03-13 23:13:39 +0000

    Task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/*
+ * Script that prints the addition of 2 integers
+ *
+ * The first argument is the first integer
+ * The second argument is the second integer
+*/
+
+let num1 = 0;
+let num2 = 0;
+
+function add(a, b) {
+  return a + b;
+}
+
+// Feeding the command arguments to the function
+num1 = parseInt(process.argv[2]);
+num2 = parseInt(process.argv[3]);
+console.log(add(num1, num2));
+
