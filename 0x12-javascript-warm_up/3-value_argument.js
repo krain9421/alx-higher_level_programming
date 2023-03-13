1f03d31fada01aaef25144580544b2c0b1af2ac2
Nwigwe Chidubem <krain9421@gmail.com>
2023-03-13 16:07:34 +0000

    Task 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/*
+ * Script that prints the first argument passed to it.
+ * If no arguments are passed to the script, print “No argument”
+*/
+
+let len = 0;
+const msg1 = 'No argument';
+
+// Getting the length of argv
+process.argv.forEach((val, index) => {
+  len += 1;
+});
+
+
+if (len === 2) {
+  console.log(msg1);
+} else {
+  console.log(process.argv[2]);
+}
