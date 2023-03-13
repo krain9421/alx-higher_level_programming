b1d1893082b8e727e43a24af4c94a863c09bcebd
Nwigwe Chidubem <krain9421@gmail.com>
2023-03-13 17:35:30 +0000

    Task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+/*
+ * Script that prints x times "C is fun"
+ *
+ * Where x is the first argument of the script
+ * If the first argument can’t be converted to an integer,
+ * print “Missing number of occurences"
+*/
+
+const msg1 = 'Missing number of occurences';
+const msg2 = 'C is fun';
+const len = process.argv.length;
+let i = 0; // Index for looping
+
+// The Loop
+if (len === 2) {
+  console.log(msg1);
+} else {
+  if (!parseInt(process.argv[2])) {
+    console.log(msg1);
+  } else {
+    for (i; i < process.argv[2]; i++) {
+      console.log(msg2);
+    }
+  }
+}
