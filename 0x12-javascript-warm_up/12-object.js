f8ede7ed27c85cc8b5c58f66f756ffefc4e9b022
Nwigwe Chidubem <krain9421@gmail.com>
2023-03-14 04:18:27 +0000

    Task 11

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+ * Script that replaces and object
+*/
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
