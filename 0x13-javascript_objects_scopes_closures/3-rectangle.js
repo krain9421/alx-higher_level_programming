ad8a49eddf603f05314e4acc42bd2326e1152010
Nwigwe Chidubem <krain9421@gmail.com>
2023-03-15 15:50:11 +0000

    Task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+/*
+ * Class Rectangle that defines a rectangle
+ * The constructor takes 2 arguments w and h
+ * Initialize the instance attribute width with the value of w
+ * Initialize the instance attribute height with the value of h
+ * If w or h is equal to 0 or not a positive integer, create
+ * an empty object
+ * Method print() prints the rectangel using the xter X
+*/
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let txt = '';
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        txt += 'X';
+      }
+      console.log(txt);
+      txt = '';
+    }
+  }
+};
