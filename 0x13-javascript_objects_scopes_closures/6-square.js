9f176e78ae875d26a87b93a7c2c0d31757858d94
Nwigwe Chidubem <krain9421@gmail.com>
2023-03-15 16:44:53 +0000

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/*
+ * Class Square that defines a square and inherits from
+ * Square of 5-square.js
+ * Method charPrint(c) prints the rectangel using the
+ * character c
+ * If c is undefined, use the character X
+*/
+
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    let txt = '';
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        txt += c;
+      }
+    console.log(txt);
+    txt = '';
+    }
+  }
+};
