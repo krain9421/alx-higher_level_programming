99f378a1a32814684ef92f9aae15dfd6f087cdd6
Nwigwe Chidubem <krain9421@gmail.com>
2023-03-19 13:46:37 +0000

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,18 +3,19 @@
 import sys
 import MySQLdb as sql
 
-# Store the argument values
-username = sys.argv[1]
-password = sys.argv[2]
-database = sys.argv[3]
+if __name__ = "__main__":
+    # Store the argument values
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-# Connecting to a MySQL database
-db = sql.connect(host="3306", user=username, passwd=password, db=database)
+    # Connecting to a MySQL database
+    db = sql.connect(host="3306", user=username, passwd=password, db=database)
 
-# Getting a Cursor
-cur = db.cursor()
+    # Getting a Cursor
+    cur = db.cursor()
 
-# Executing the MySQL Queries
-curr.execute("SELECT `name` FROM states ORDER BY `id` DESC;")
-rows = c.fetchall()
-print(state) for state in rows
+    # Executing the MySQL Queries
+    curr.execute("SELECT `name` FROM states ORDER BY `id` DESC;")
+    rows = c.fetchall()
+    print(state) for state in rows
