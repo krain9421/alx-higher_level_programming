a3f7c18b189956063afa5adbcee2c31682b71245
Nwigwe Chidubem <krain9421@gmail.com>
2023-03-19 14:35:39 +0000

    Task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Script that lists all states with a name
+    starting with `N` from database hbtn_0e_0_usa.
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    # Store the argument values
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connecting to a MySQL database
+    db = MySQLdb.connect(user=username, passwd=password, db=database)
+
+    # Getting a Cursor
+    cur = db.cursor()
+
+    # Executing the MySQL Queries
+    # cur.execute("SELECT `name` FROM states ORDER BY `id` ASC;")
+    cur.execute("SELECT * FROM `states` WHERE name LIKE 'N%';")
+    rows = cur.fetchall()
+    for row in rows:
+        print("{}".format(row))
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
