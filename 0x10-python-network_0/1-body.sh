d8b477039857d12593e8a3d9ca70fd86a2b50e81
Nwigwe Chidubem <krain9421@gmail.com>
2023-03-31 02:49:10 +0000

    Modified Task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a GET request to that URL, and displays the body of the response
-curl -sX GET $1 -L
+response=$(curl -s -w "%{http_code}" -o >(cat - >&2) $1); [ "$response" == "200" ] && curl -s $1
