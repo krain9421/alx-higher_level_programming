9b0fb928b32e6197dff056dabb549cfc3c568a31
Nwigwe Chidubem <krain9421@gmail.com>
2023-04-04 16:39:03 +0000

    What's my status?

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Script that fetches the URL `https://intranet.hbtn.io/status """
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(req) as response:
+        content = response.read()
+        content_type = type(content)
+        content_utf = content.decode("UTF-8")
+
+        print("Body response:\
+        \n\t- type: {}\
+        \n\t- content: {}\
+        \n\t- utf8 content: {}".format(content_type, content, content_utf))
