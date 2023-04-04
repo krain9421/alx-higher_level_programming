4959e6370ec983c4908aed82ebfcda734c3a3774
Nwigwe Chidubem <krain9421@gmail.com>
2023-04-04 17:59:50 +0000

    What's my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,12 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request("https://intranet.hbtn.io/status")
+    # req = urllib.request.Request("https://intranet.hbtn.io/status")
     # req2 = urllib.request.Request("http://0.0.0.0:5050/status")
 
     # Handling URLError
     try:
-        with urllib.request.urlopen(req) as response:
+        with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
             content = response.read()
             content_type = type(content)
             content_utf = content.decode("UTF-8")
