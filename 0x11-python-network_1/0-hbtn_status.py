2eda1968af7ee7702473c92905fda7f63f625446
Nwigwe Chidubem <krain9421@gmail.com>
2023-04-04 17:14:21 +0000

    What's my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,10 +7,10 @@ if __name__ == "__main__":
     req = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(req) as response:
         content = response.read()
-        # content_type = type(content)
-        # content_utf = content.decode("UTF-8")
+        content_type = type(content)
+        content_utf = content.decode("UTF-8")
 
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode("UTF-8")))
+    print("Body response:")
+    print("\t- type: {}".format(content_type))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content_utf))
