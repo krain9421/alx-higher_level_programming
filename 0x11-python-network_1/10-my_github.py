3ef0db6881b031247aae61da63fba7ad9a939111
Nwigwe Chidubem <krain9421@gmail.com>
2023-04-05 00:29:13 +0000

    Task 9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -4,16 +4,16 @@ Script that uses the Github API to display a user's Github id.
 """
 
 import requests
-import argv
+import sys
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     user = sys.argv[1]
     passwd = sys.argv[2]
     url = 'https://api.github.com/user'
     auth = (user, passwd)
     # Getting the HTTP response
-    response = requests.get(url, auth)
+    response = requests.get(url, auth=(user, passwd))
     res_json = response.json()
     git_id = res_json.get('id')
 
