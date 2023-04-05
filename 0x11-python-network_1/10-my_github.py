8971d947ec9f56c07e01a67551e02657a018d05b
Nwigwe Chidubem <krain9421@gmail.com>
2023-04-05 00:24:46 +0000

    Task 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script that uses the Github API to display a user's Github id.
+"""
+
+import requests
+import argv
+
+
+if __name__ = "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    url = 'https://api.github.com/user'
+    auth = (user, passwd)
+    # Getting the HTTP response
+    response = requests.get(url, auth)
+    res_json = response.json()
+    git_id = res_json.get('id')
+
+    print(git_id)
